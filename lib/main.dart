47cdad33665253e87bb7842dd1374fa221a5ffae
ogen3 <saltgenki@gmail.com>
2021-01-05 16:12:02 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'uchinoko ogen',
       theme: ThemeData(
         // This is the theme of your application.
         //
